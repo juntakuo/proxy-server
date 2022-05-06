b1a43e54714755e523f29933bfa51ddfab455ecf
Tsu-Hao Kuo <tkuo@snapchat.com>
2022-05-06 12:51:21 -0700

    more fix

--- bin/client.js
+++ bin/client.js
@@ -27,14 +27,14 @@ class Client {
     respondJSON(msg) {
         this.respond(200, { 'Content-Type': 'application/json' }, JSON.stringify(msg));
     }
-    hostUpdatedSource(src) {
+    hostUpdatedSource(src, addedPath = '') {
         const host = this.req.headers['host'];
         if (!host) {
             this.respond(500, { 'Content-Type': 'text/html' }, '500 - Server error (missing hostname)');
             return;
         }
-        return src.replace(Client.hostReplaceRx, 'https://' + host);
+        return src.replace(Client.hostReplaceRx, `https://${host}${addedPath}`);
     }
 }
 exports.Client = Client;
-Client.hostReplaceRx = /(((http:\/\/((192\.168\.[0-9]{1,3}\.[0-9]{1,3})|localhost):[0-9]{1,4})|LOCAL_SERVER_URL)\/r)|\{*\**HOST_URL_GOES_HERE\**\}*/g;
+Client.hostReplaceRx = /(((http:\/\/((192\.168\.[0-9]{1,3}\.[0-9]{1,3})|localhost):[0-9]{1,4})|LOCAL_SERVER_URL))|\{*\**HOST_URL_GOES_HERE\**\}*/g;
--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,7 @@
 import http from 'http';
 
 export class Client {
-  protected static readonly hostReplaceRx = /(((http:\/\/((192\.168\.[0-9]{1,3}\.[0-9]{1,3})|localhost):[0-9]{1,4})|LOCAL_SERVER_URL)\/r)|\{*\**HOST_URL_GOES_HERE\**\}*/g;
+  protected static readonly hostReplaceRx = /(((http:\/\/((192\.168\.[0-9]{1,3}\.[0-9]{1,3})|localhost):[0-9]{1,4})|LOCAL_SERVER_URL))|\{*\**HOST_URL_GOES_HERE\**\}*/g;
 
   constructor(protected readonly req: http.IncomingMessage, protected readonly res: http.ServerResponse) {
     console.log('req', req.method, req.url);
@@ -29,12 +29,12 @@ export class Client {
     this.respond(200, { 'Content-Type': 'application/json' }, JSON.stringify(msg));
   }
 
-  protected hostUpdatedSource(src: string): string | undefined {
+  protected hostUpdatedSource(src: string, addedPath = ''): string | undefined {
     const host = this.req.headers['host'];
     if (!host) {
       this.respond(500, { 'Content-Type': 'text/html' }, '500 - Server error (missing hostname)')
       return;
     }
-    return src.replace(Client.hostReplaceRx, 'https://' + host);
+    return src.replace(Client.hostReplaceRx, `https://${host}${addedPath}`);
   }
 }
