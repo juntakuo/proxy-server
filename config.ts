dc1f19b9e0199f89b69eaca79af827298adddd98
tkuosc <tkuo@snapchat.com>
2022-05-06 11:06:42 -0700

    Update config.ts

--- config.ts
+++ config.ts
@@ -3,6 +3,6 @@ export const config = {
   // js: 'https://sc-static.net/scevent.min.js',
   js: 'static/scevent.min.js',
   pixelUrl: 'https://tr-shadow.snapchat.com/gateway/p',
-  serverHost: 'localhost',
+  serverHost: '0.0.0.0',
   serverPort: 8080
 };
